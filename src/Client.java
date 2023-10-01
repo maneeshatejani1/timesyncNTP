ee118fab8b09ebc37f6a90830cc1110f1b67a7b8
Maneesha Tejani <maneeshatejani@gmail.com>
2023-10-01 14:12:20 -0400

    fixed errors and made some changes

--- src/Client.java
+++ src/Client.java
@@ -7,63 +7,41 @@ import java.time.temporal.ChronoUnit;
 
 public class Client implements Serializable{
     private static long synchronizedTime;
-    public static class Payload{
-        private Instant t1; 
-        private Instant t2; 
-        private Instant t3; 
-        private Instant t4; 
-        public Payload(Payload payload, Instant t4){
-            this.t1 = payload.t1;
-            this.t2 = payload.t2;
-            this.t3 = payload.t3;
-            this.t4 = t4;
-        }
-        public Instant getT1(){
-            return this.t1;
-        }
-        public Instant getT2(){
-            return this.t2;
-        }
-        public Instant getT3(){
-            return this.t3;
-        }
-        public Instant getT4(){
-            return this.t4;
-        }
-    }
 
-    private static long calculateOffset(Payload finalResponse, long currentTimeMillis){
-        return currentTimeMillis + Math.round((ChronoUnit.MILLIS.between(finalResponse.t3, finalResponse.t4) 
-                            - ChronoUnit.MILLIS.between(finalResponse.t1, finalResponse.t2))
+    private static long calculateOffset(Instant t1, Instant t2, Instant t3, Instant t4, long currentTimeMillis){
+        return currentTimeMillis + Math.round((ChronoUnit.MILLIS.between(t3, t4) 
+                            - ChronoUnit.MILLIS.between(t1, t2))
                             *0.5);
     }
-    private static long calculateDelay(Payload finalResponse){
-        return ChronoUnit.MILLIS.between(finalResponse.t1, finalResponse.t4) 
-                            - ChronoUnit.MILLIS.between(finalResponse.t2, finalResponse.t3);
+    private static long calculateDelay(Instant t1, Instant t2, Instant t3, Instant t4){
+        return ChronoUnit.MILLIS.between(t1, t4) 
+                            - ChronoUnit.MILLIS.between(t2, t3);
     }
     public static void main(String[] args) throws Exception {
 		Socket clientSocket = new Socket("localhost", 6789);
         System.out.println("Socket created.....");
 		ObjectOutputStream outToServer = new ObjectOutputStream(
 				clientSocket.getOutputStream());
-        System.out.println("Out To Server created.....");
-
-        ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
-
-		outToServer.writeObject(Instant.now());
+        System.out.println("Out To Server created.....");        
+        Instant t1 = Instant.now();
+		outToServer.writeObject(t1);
         outToServer.flush();
-        //outToServer.close();
+        
         System.out.println("Object written to server.....");
-        Payload x = (Payload) inFromServer.readObject();
-		Payload finalResponse = new Payload(x, Instant.now());
-        System.out.println("response obtained from server......");
+        ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
+        
+        Instant timestamps[] = (Instant[])inFromServer.readObject();
+        Instant t2 = timestamps[0];
+        Instant t3 = timestamps[1];
+        Instant t4 = Instant.now();
 
-        //inFromServer.close();
+        outToServer.close();
+        inFromServer.close();
         long currentTimeMillis = System.currentTimeMillis();
-        synchronizedTime = calculateOffset(finalResponse, currentTimeMillis);
-        System.out.println("REMOTE_TIME "+synchronizedTime);
-        System.out.println("LOCAL_TIME "+currentTimeMillis);
-        System.out.println("RTT_ESTIMATE "+calculateDelay(finalResponse));
+        synchronizedTime = calculateOffset(t1, t2, t3, t4, currentTimeMillis);
+        System.out.println("REMOTE_TIME "+ synchronizedTime);
+        System.out.println("LOCAL_TIME "+ currentTimeMillis);
+        System.out.println("RTT_ESTIMATE "+ calculateDelay(t1, t2, t3, t4));
         clientSocket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -6,39 +6,33 @@ import java.net.Socket;
 import java.time.Instant;
 
 public class Server {
-    public static class Payload implements Serializable{
-        private Instant t1; 
-        private Instant t2; 
-        private Instant t3; 
-        private Instant t4; 
-
-        public Payload(Instant t1, Instant t2){
-            this.t1 = t1;
-        }
-
-        public Payload(Payload payload, Instant t3){
-            this.t1 = payload.t1;
-            this.t2 = payload.t2;
-            this.t3 = t3;
-        }
-    }
     public static void main(String[] args) throws Exception {
 		ServerSocket welcomeSocket = new ServerSocket(6789);
         Socket connectionSocket = welcomeSocket.accept();
         System.out.println("Connection established");
+
         ObjectInputStream inFromClient = new ObjectInputStream(connectionSocket.getInputStream());
         System.out.println("In from Client created......");
-        Instant x = (Instant)inFromClient.readObject();
-        Payload request = new Payload(x, Instant.now());
-        System.out.println("Request obtained from client......");
-        ///inFromClient.close();
+
         ObjectOutputStream outToClient = new ObjectOutputStream(
                     connectionSocket.getOutputStream());
-        outToClient.writeObject(new Payload(request, Instant.now())); 
+        System.out.println("Out To Client created.....");
+
+        Instant t1 = (Instant)inFromClient.readObject();
+        System.out.println(t1 + "t1 received");
+        Instant t2 = Instant.now();
+
+        Instant timestamps[] = new Instant[2];
+        timestamps[0] = t1;
+        timestamps[1] = Instant.now();
+
+        outToClient.writeObject(timestamps); 
         outToClient.flush();
-       // outToClient.close();
-       connectionSocket.close();
-       welcomeSocket.close();
+
+        outToClient.close();
+        inFromClient.close();
+        connectionSocket.close();
+        welcomeSocket.close();
             
     }
 }
