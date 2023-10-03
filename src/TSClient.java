110c7bf1e28baf9b53b5fd6765fef75b27323a6e
Prateek Bhindwar <pbprateeek@gmail.com>
2023-10-03 06:16:50 +0530

    documentation for code

--- src/TSClient.java
+++ src/TSClient.java
@@ -6,14 +6,17 @@ import java.net.Socket;
 public class TSClient implements Serializable{
     private static long synchronizedTime; //Separate variable maintaining time synchronized with server
 
+    //Method to calculate offset (NTP)
     private static long calculateOffset(long t1, long t2, long t3, long t4){
         return Math.round(((t4-t3) - (t2-t1)) * 0.5);
     }
+
+    //Method to calculate Round Trip Estimate (NTP)
     private static long calculateDelay(long t1, long t2, long t3, long t4){
         return (t4-t1) - (t3-t2);
     }
     public static void main(String[] args) throws Exception {
-        String hostIp;
+        String hostIp; // Capturing hostname of remote server
         if(args.length == 0) {
             hostIp = "localhost";
         }
@@ -24,13 +27,16 @@ public class TSClient implements Serializable{
         long curr_offset, curr_delay, min_delay = Integer.MAX_VALUE;
         long[][] timestamps;
         long t1,t2,t3,t4,currentTimeMillis = System.currentTimeMillis();
-        for(int i=0;i<8;i++){
-            Socket clientSocket = new Socket(hostIp, 14886);
+        for(int i=0;i<8;i++){       //Pinging server 8 times to get min. delay
+            Socket clientSocket = new Socket(hostIp, 14886); // Connecting to the server on port 14886
             ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
             ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
-            outToServer.writeObject(System.currentTimeMillis());
+            outToServer.writeObject(System.currentTimeMillis()); // Sending value of T1 to the server
             outToServer.flush();
+
+            //Capturing T2 and T3 from the server while recording local time T4
             timestamps = new long[][]{(long[])inFromServer.readObject(), new long[]{System.currentTimeMillis()}};
+            
             t1 = timestamps[0][0];
             t2 = timestamps[0][1];
             t3 = timestamps[0][2];
@@ -38,6 +44,8 @@ public class TSClient implements Serializable{
 
             curr_offset = calculateOffset(t1, t2, t3, t4);
             curr_delay = calculateDelay(t1, t2, t3, t4);
+
+            //Calculating offset for the min. delay
             if(curr_delay < min_delay){
                 min_delay = curr_delay;
                 currentTimeMillis = t4;
@@ -48,8 +56,8 @@ public class TSClient implements Serializable{
             clientSocket.close();
         }
         
-        System.out.println("REMOTE_TIME "+ synchronizedTime);
-        System.out.println("LOCAL_TIME "+ currentTimeMillis);
+        System.out.println("REMOTE_TIME "+ synchronizedTime); // Remote time = (Server's time (T3) + Offset)
+        System.out.println("LOCAL_TIME "+ currentTimeMillis); // Local time = Client's time (T4)
         System.out.println("RTT_ESTIMATE "+ min_delay);
     }
 }
--- src/TSServer.java
+++ src/TSServer.java
@@ -7,11 +7,11 @@ import java.net.Socket;
 
 public class TSServer implements Serializable {
     public static void main(String[] args) throws Exception {
-		ServerSocket welcomeSocket = new ServerSocket(14886);
+		ServerSocket welcomeSocket = new ServerSocket(14886); // Opening server socket on port 14886
         System.out.println("Server started. Accepting Connections");
         try{
             while(true){
-                new ClientHandler(welcomeSocket.accept()).start();
+                new ClientHandler(welcomeSocket.accept()).start(); // Accepting incoming client connection, parallely
                 System.out.println("Client Connected");
             }
             
@@ -25,7 +25,7 @@ public class TSServer implements Serializable {
             
     }
 
-    private static class ClientHandler extends Thread {
+    private static class ClientHandler extends Thread { // Multi-threaded implementation
         private Socket connectionSocket;
         private ObjectInputStream inFromClient;
         private ObjectOutputStream outToClient; 
@@ -45,7 +45,10 @@ public class TSServer implements Serializable {
 
         public void run() {
             try{
+                //Capturing T1 from client and recording T2 at server
                 timestamps = new long[]{(long)inFromClient.readObject(), System.currentTimeMillis()};
+
+                //Sending T1, T2, T3 after recording T3 at server
                 outToClient.writeObject(new long[]{timestamps[0],timestamps[1], System.currentTimeMillis()});
                 outToClient.flush();
                 outToClient.close();
